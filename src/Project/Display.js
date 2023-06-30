7ecd8b083188ced59369005905131f0ebd89106f
Abdela Umer <adheremo65@gmail.com>
2023-06-29 21:04:52 -0400

    sourc accepting tag add

--- src/Project/Display.js
+++ src/Project/Display.js
@@ -3,21 +3,19 @@ import { useState, useEffect } from "react";
 export default function Display() {
   const [listItem, setListItem] = useState([]);
   useEffect(() => {
-    const list = () => {
-      const fetch = () =>
-        fetch("http://localhost:3000/Note").then((res) => res.json());
+    const output = () =>
+      fetch("http://localhost:3000/Note").then((res) => res.json());
+    async function list() {
+      const listData = await output();
+      setListItem(listData);
+    }
+    list();
+  });
 
-      async function list() {
-        const listData = await fetch();
-        setListItem(listData);
-      }
-      list();
-    };
-  }, []);
   const items = listItem.map((item) => (
-    <section>
-      <header>{item.Topic}</header>
-      <h1>{item.Title}</h1>
+    <section key={item.id}>
+      <header>{item.Title}</header>
+      <h1>{item.Topic}</h1>
     </section>
   ));
   return <div>{items}</div>;
